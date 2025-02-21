f94ab8d851a7f1a87d3343a3c8861a91e48c2344
David Arias Vázquez <dav.surf@gmail.com>
2025-02-21 17:49:24 +0100

    fix: Swap bag icon when cart is not empty

--- src/components/app_bar/cart_counter.tsx
+++ src/components/app_bar/cart_counter.tsx
@@ -1,9 +1,15 @@
 import styles from "../app_bar.module.scss";
 import Link from "next/link";
+import {clsx} from "clsx";
 
 export default function CartCounter() {
+  const classes = clsx(
+    styles.app_bar__cart_counter,
+    styles['app_bar__cart_counter--empty'], // TODO - Add logic.
+  )
+
   return (
-    <Link href='/cart' aria-label='Go to the cart' className={styles.app_bar__cart_counter}>
+    <Link href='/cart' aria-label='Go to the cart' className={classes}>
       <span aria-hidden={true} className={styles.app_bar__cart_counter__icon}></span>
       <span aria-label='Number of products in the cart' className={styles.app_bar__cart_counter__label}>0</span>
     </Link>
